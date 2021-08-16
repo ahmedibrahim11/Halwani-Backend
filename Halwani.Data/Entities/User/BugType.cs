08d11acccbd61766fc5b6738380e07cee2e6c179
Mostafa <mostafa.ibrahim2@vodafone.com>
2021-08-16 21:27:32 +0200

    update-report-bug-support

---
+++ Halwani.Data/Entities/User/BugType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Halwani.Data.Entities.User
+{
+    public enum BugType
+    {
+        technical=0,
+        nonTechnical,
+    }
+}
